8a98ade50066bc1dc52809b47c7b52efda5c0a1f
MMaaxxxii <maximilian.nestle@hs-weingarten.de>
2017-07-18 20:54:05 +0200

    umbennenung

--- Computergrafik/Computergrafik/locig.cs
+++ Computergrafik/Computergrafik/locig.cs
@@ -6,12 +6,12 @@ using System.Threading.Tasks;
 
 namespace Computergrafik
 {
-    class Program
+    class locig
     {
         static void Main(string[] args)
         {
 
-            //geändert
+            //geändert rwjgjlkjjkjkjk
 
         }
     }
