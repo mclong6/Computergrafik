cbd8c871f73a10d3179f8f789e3c25b9f4a5addb
McLong <ml-153022@hs-weingarten.de>
2017-07-18 20:17:07 +0200

    Versuch neues Projekt angelegt!

---
+++ Computergrafik/Computergrafik/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Computergrafik
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
