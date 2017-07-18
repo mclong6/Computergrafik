d418d47e803e5ecd91176bbf3e698749a68de63b
Sam <sf-153040@hs-weingarten.de>
2017-07-18 21:00:26 +0200

    zweiter Test frizt

---
+++ Computergrafik/Computergrafik/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Computergrafik
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //geändert
+            //gege
+            //test
+        }
+    }
+}
