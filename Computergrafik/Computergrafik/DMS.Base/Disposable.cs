933e3099fc5fc1181ee0855d11f1351850d75a23
SamFri <sf-153040@hs-weingarten.de>
2017-07-19 11:08:27 +0200

    Add files via upload

---
+++ Computergrafik/Computergrafik/DMS.Base/Disposable.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace DMS.Base
+{
+	public abstract class Disposable : IDisposable
+	{
+		protected abstract void DisposeResources();
+
+		public bool Disposed { get{ return disposed; } }
+
+		/// <summary>
+		/// Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
+		/// </summary>
+		public void Dispose()
+		{
+			Dispose(true);
+			GC.SuppressFinalize(this);
+		}
+
+		private void Dispose(bool disposing)
+		{
+			if (disposed) return;
+			if (disposing)
+			{
+				DisposeResources();
+				disposed = true;
+			}
+		}
+
+		private bool disposed = false;
+	}
+}
