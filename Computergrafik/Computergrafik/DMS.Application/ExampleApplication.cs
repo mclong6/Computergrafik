0c2a4bc03fa3e723c804d5111c000b41fa674d92
SamFri <sf-153040@hs-weingarten.de>
2017-07-19 10:59:35 +0200

    Add files via upload

---
+++ Computergrafik/Computergrafik/DMS.Application/ExampleApplication.cs
@@ -0,0 +1,125 @@
+﻿using DMS.Base;
+using DMS.OpenGL;
+using OpenTK;
+using OpenTK.Graphics.OpenGL;
+using OpenTK.Input;
+using OpenTK.Platform;
+using System;
+using System.ComponentModel.Composition;
+using System.ComponentModel.Composition.Hosting;
+
+namespace DMS.Application
+{
+	public class ExampleApplication
+	{
+		public ExampleApplication(int width = 512, int height = 512, double updateRate = 60)
+		{
+			var catalog = new AggregateCatalog();
+			catalog.Catalogs.Add(new AssemblyCatalog(typeof(ExampleApplication).Assembly));
+			_container = new CompositionContainer(catalog);
+			try
+			{
+				_container.SatisfyImportsOnce(this);
+			}
+			catch (CompositionException e)
+			{
+				Console.WriteLine(e.ToString());
+			}
+
+			gameWindow = new GameWindow(width, height);
+			gameWindow.TargetUpdateFrequency = updateRate;
+			gameWindow.TargetRenderFrequency = updateRate;
+			gameWindow.VSync = VSyncMode.On;
+			//register callback for resizing of window
+			gameWindow.Resize += GameWindow_Resize;
+			//register callback for keyboard
+			gameWindow.KeyDown += GameWindow_KeyDown;
+			gameWindow.KeyDown += (sender, e) => { if (Key.Escape == e.Key) gameWindow.Exit(); };
+			ResourceManager = resourceProvider as ResourceManager;
+		}
+
+		public IGameWindow GameWindow { get { return gameWindow; } }
+
+		public event Action Render;
+
+		public delegate void ResizeHandler(int width, int height);
+		public event ResizeHandler Resize;
+
+		public delegate void UpdateHandler(float updatePeriod);
+		public event UpdateHandler Update;
+
+		public bool IsRecording
+		{
+			get { return !ReferenceEquals(null, frameListCreator); }
+			set
+			{
+				if (!ReferenceEquals(null, frameListCreator)) return;
+				frameListCreator = value ? new FrameListCreator(gameWindow.Width, gameWindow.Height) : null;
+			}
+		}
+
+		public ResourceManager ResourceManager { get; private set; }
+
+		public System.Numerics.Vector2 CalcNormalized(int pixelX, int pixelY)
+		{
+			return new System.Numerics.Vector2(pixelX / (gameWindow.Width - 1f), 1f - pixelY / (gameWindow.Height - 1f));
+		}
+
+		public void Run()
+		{
+			//register a callback for updating the game logic
+			gameWindow.UpdateFrame += (sender, e) => Update?.Invoke((float)gameWindow.TargetUpdatePeriod);
+			//registers a callback for drawing a frame
+			gameWindow.RenderFrame += (sender, e) => GameWindowRender();
+			//run the update loop, which calls our registered callbacks
+			gameWindow.Run();
+		}
+
+		private CompositionContainer _container;
+		private GameWindow gameWindow;
+		private FrameListCreator frameListCreator;
+
+		[Import]
+		private IResourceProvider resourceProvider = null;
+
+		private void GameWindowRender()
+		{
+			ResourceManager?.CheckForShaderChange();
+			//record frame
+			frameListCreator?.Activate();
+			//render
+			Render?.Invoke();
+			//stop recording frame
+			frameListCreator?.Deactivate();
+			//buffer swap of double buffering (http://gameprogrammingpatterns.com/double-buffer.html)
+			gameWindow.SwapBuffers();
+			//save frame
+			frameListCreator?.SaveFrame();
+		}
+
+		private void GameWindow_KeyDown(object sender, KeyboardKeyEventArgs e)
+		{
+			switch (e.Key)
+			{
+				case Key.Escape:
+					if (!ReferenceEquals(null, frameListCreator)) frameListCreator.Frames.SaveToDefaultDir();
+					gameWindow.Exit();
+					break;
+				case Key.F11:
+					gameWindow.WindowState = WindowState.Fullscreen == gameWindow.WindowState ? WindowState.Normal : WindowState.Fullscreen;
+					break;
+			}
+		}
+
+		private void GameWindow_Resize(object sender, EventArgs e)
+		{
+			GL.Viewport(0, 0, gameWindow.Width, gameWindow.Height);
+			if (!ReferenceEquals(null, frameListCreator))
+			{
+				frameListCreator.Dispose();
+				frameListCreator = new FrameListCreator(gameWindow.Width, gameWindow.Height);
+			}
+			Resize?.Invoke(gameWindow.Width, gameWindow.Height);
+		}
+	}
+}
---
+++ Computergrafik/Computergrafik/DMS.Application/GameWindowExtensions.cs
@@ -0,0 +1,35 @@
+﻿using DMS.Geometry;
+using OpenTK;
+using OpenTK.Input;
+using OpenTK.Platform;
+using System;
+
+namespace DMS.Application
+{
+	public static class GameWindowExtensions
+	{
+		public static void ConnectEvents(this INativeWindow gameWindow, CameraOrbit camera)
+		{
+			gameWindow.Resize += (s, e) => camera.Aspect = (float)gameWindow.Width / gameWindow.Height;
+			gameWindow.MouseMove += (s, e) =>
+			{
+				if (ButtonState.Pressed == e.Mouse.LeftButton)
+				{
+					camera.Azimuth += 300 * e.XDelta / (float)gameWindow.Width;
+					camera.Elevation += 300 * e.YDelta / (float)gameWindow.Height;
+				}
+			};
+			gameWindow.MouseWheel += (s, e) =>
+			{
+				if (Keyboard.GetState().IsKeyDown(Key.ShiftLeft))
+				{
+					camera.FovY *= (float)Math.Pow(1.05, e.DeltaPrecise);
+				}
+				else
+				{
+					camera.Distance *= (float)Math.Pow(1.05, e.DeltaPrecise);
+				}
+			};
+		}
+	}
+}
