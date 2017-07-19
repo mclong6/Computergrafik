a1d4fad2e8cc2b8ba53f9bed88a7d3c452bb51ec
Sam <sf-153040@hs-weingarten.de>
2017-07-19 14:06:16 +0200

    XBox klasse

--- Computergrafik/Computergrafik/Model.cs
+++ Computergrafik/Computergrafik/Model.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DMS.Geometry;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,6 @@ namespace Computergrafik
 {
     class Model
     {
+        Box2D box;
     }
 }
