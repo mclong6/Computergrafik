9704fc763f5f934c6d399f7dc3ab34d90b10acd0
McLong <ml-153022@hs-weingarten.de>
2017-07-27 17:41:24 +0200

    level,opponent

--- Computergrafik/Computergrafik/Visuals.cs
+++ Computergrafik/Computergrafik/Visuals.cs
@@ -133,15 +133,30 @@ namespace Computergrafik
         }
         public void DrawOpponent(float centerX, float centerY, float radius)
         {
+         
+
             GL.Begin(PrimitiveType.Polygon);
-            GL.Color3(Color.Olive);
+            GL.Color3(Color.OrangeRed);
+               
+                for (float alpha = 0.0f; alpha < 2 * Math.PI; alpha += 0.1f * (float)Math.PI)
+                {
+                    float x = radius * (float)Math.Cos(alpha);
+                    float y = radius * (float)Math.Sin(alpha);
+                    GL.Vertex2(centerX + x, centerY + y);
+                }
+            
+            GL.End();
+            GL.Begin(PrimitiveType.Polygon);
+            float radiusSmall = radius - 0.005f;
+            GL.Color3(Color.DarkSlateGray);
             for (float alpha = 0.0f; alpha < 2 * Math.PI; alpha += 0.1f * (float)Math.PI)
             {
-                float x = radius * (float)Math.Cos(alpha);
-                float y = radius * (float)Math.Sin(alpha);
+                float x = radiusSmall * (float)Math.Cos(alpha);
+                float y = radiusSmall * (float)Math.Sin(alpha);
                 GL.Vertex2(centerX + x, centerY + y);
             }
             GL.End();
+
         }
 
         public Texture BackgroundTexture
